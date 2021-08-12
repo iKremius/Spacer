ce0c53a1fd1b884bfb2e85526dd95ee4c1a22e98
Adam Korytkowski <adam.korytkowski@icloud.com>
2021-08-12 19:31:05 +0200

    Przeniesiono tło aplikacji do osobnego komponentu 'HeroImage'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrapper">
+    <heroImage />
     <claim />
     <searchInput />
   </div>
 </template>
 
 <script>
+import HeroImage from '@/components/HeroImage.vue';
 import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
 
 export default {
   components: {
+    HeroImage,
     Claim,
     SearchInput,
   },
@@ -35,14 +38,11 @@ body {
   margin: 0;
   width: 100%;
   height: 100vh;
+  min-height: 100vh;
   padding: 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-image: url('./assets/background.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 80% 0%;
 }
 </style>
